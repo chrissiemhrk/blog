61e3086aec806ef1d913821888018f5a350fc819
chrissiemhrk <chrissiemhrk@gmail.com>
2021-01-03 18:08:11 +0200

    style: add stylings

--- blog/src/components/PostList.vue
+++ blog/src/components/PostList.vue
@@ -1,36 +1,40 @@
 <template>
   <div class="post-list">
-    <h1>{{year}}</h1>
-    <PostItem :key="post.node.id" v-for="post in this.postsByYear" :post="post.node" />
+    <h1>{{ year }}</h1>
+    <PostItem
+      :key="post.node.id"
+      v-for="post in this.postsByYear"
+      :post="post.node"
+    />
   </div>
 </template>
 
 <script>
-import PostItem from './PostItem';
+import PostItem from "./PostItem";
 
 export default {
   props: ["year"],
   components: {
-    PostItem
+    PostItem,
   },
   computed: {
     postsByYear() {
       const posts = this.$page.allPost.edges;
       return posts.filter((post) => {
         return post.node.date.includes(this.year);
-      })
-    }
-  }
+      });
+    },
+  },
 };
 </script>
 
 <style scoped="true">
 h1 {
-  font-size:2em;
-  margin:0;
+  font-size: 1.5rem;
+  margin: 2rem 0;
 }
 
 .post-list {
-  margin-top:20px;
+  margin-bottom: 1.5rem;
 }
 </style>
