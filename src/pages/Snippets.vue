27aebece5f2c2eeae19720bc2925ea38c53a79eb
chrissiemhrk <chrissiemhrk@gmail.com>
2021-01-27 11:04:46 +0200

    feat: add snippets page

---
+++ src/pages/Snippets.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+    <section class="snippets">
+      <h1>Comming soon</h1>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Snippets",
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  font-size: 1.8rem;
+  margin: 2rem 0;
+  position: sticky;
+  text-transform: uppercase;
+}
+
+h3 {
+  font-weight: 500;
+}
+</style>
